126eb478e4c14dfdfa7fa7361cd00d3a105d7e32
Devansh Agrawal <devagr3924@gmail.com>
2021-03-01 18:51:27 -0600

    Added candlestick chart functionality

--- part2scraper.py
+++ part2scraper.py
@@ -2,6 +2,9 @@ import codecs, requests, re
 from bs4 import BeautifulSoup
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
+from mplfinance.original_flavor import candlestick2_ohlc
+import matplotlib.ticker as ticker
+import datetime as datetime
 import numpy as np
 
 #The project will be analyzing the gme stock over 1 month.
@@ -49,6 +52,7 @@ stocks.add_rows(data)
 
 print(stocks)
 
+'''
 dates = list()
 
 #gets the numerical date in the month from the date in the table
@@ -68,14 +72,70 @@ for the_date in data:
 #reverses dates from oldest to newest for graph
 dates.reverse()
 print(dates)
+'''
 
 the_opens = list()
 for an_item in data:
     the_open = an_item[1]
     the_opens.append(float(the_open))
 
-the_opens.reverse()
-print(the_opens)
+the_closes = list()
+for an_item in data:
+    the_close = an_item[2]
+    the_closes.append(float(the_close))
 
-plt.plot(dates, the_opens)
+the_highs = list()
+for an_item in data:
+    the_high = an_item[3]
+    the_highs.append(float(the_high))
+
+the_lows = list()
+for an_item in data:
+    the_low = an_item[4]
+    the_lows.append(float(the_low))
+
+the_opens.reverse()
+the_closes.reverse()
+the_highs.reverse()
+the_lows.reverse()
+
+print(the_opens)
+print(the_closes)
+print(the_highs)
+print(the_lows)
+
+
+'''
+print(type(data[1][1]))
+
+dates = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+'''
+print(data)
+
+fig, ax = plt.subplots()
+candlestick2_ohlc(ax, the_opens, the_closes, the_highs, the_lows, width=0.6, colorup='g', colordown='r', alpha=0.75)
+
+#candlestick2_ohlc(ax, the_opens, data[2], data[3], data[4], width=0.6)
+'''
+xdate = [datetime.datetime.fromtimestamp(i) for i in dates]
+
+ax.xaxis.set_major_locator(ticker.MaxNLocator(6))
+def mydate(x,pos):
+    try:
+        return xdate[int(x)]
+    except IndexError:
+        return ''
+
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(mydate))
+
+fig.autofmt_xdate()
+fig.tight_layout()
+'''
+plt.xlabel('Days from 14 day mark')
+plt.ylabel('Candle for each day')
+plt.title(f'Candlestick chart of {the_stock.upper()}')
 plt.show()
+
+
+'''plt.plot(dates, the_opens, 'ro')
+plt.show()'''
